3a7e439fce2a0a2f6471a93d823eeb5eedfa1f30
allenyllee <allen7575@gmail.com>
2017-11-20 18:06:46 +0800

    remove unnecessary export

--- run.sh
+++ run.sh
@@ -5,20 +5,9 @@
 
 # docker run $1 ${@:2}
 
-XSOCK=/tmp/.X11-unix
-XAUTH_DIR=/tmp/.docker.xauth
-XAUTH=$XAUTH_DIR/.xauth
-
-PORT=8081
-
 docker run -ti --rm \
     --name motion \
-    --publish $PORT:8081 \
-    --env DISPLAY=$DISPLAY \
-    --env XAUTHORITY=$XAUTH \
-    --volume $XSOCK:$XSOCK \
-    --volume $XAUTH_DIR:$XAUTH_DIR \
-    --volume $PWD/scripts:/scripts2 \
+    --publish 8081:8081 \
     --device /dev/video0:/dev/video0 `# for webcam` \
     --device /dev/video1:/dev/video1 `# for webcam` \
     allenyllee/motion-webcam-server

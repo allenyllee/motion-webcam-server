6da0c1ac7168119ecb8f935bf6738328821c5473
allenyllee <allen7575@gmail.com>
2017-11-20 23:04:35 +0800

    modify run script to suport multiple camera

--- run.sh
+++ run.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-# shell - Process all arguments except the first one (in a bash script) - Stack Overflow
-# https://stackoverflow.com/questions/9057387/process-all-arguments-except-the-first-one-in-a-bash-script
+#
+# usage:
+#       ./run.sh [container_name] [port] [video path]
+# ex:
+#   ./run.sh motion0 8081 /dev/video0
+#   ./run.sh motion1 8082 /dev/video1
+#
 
-# docker run $1 ${@:2}
+
+CONTAINER_NAME=$1
+OUT_PORT=$2
+VIDEO_PATH=$3
 
 docker run -ti --rm \
-    --name motion \
-    --publish 8081:8081 \
-    --device /dev/video0:/dev/video0 `# for webcam` \
-    --device /dev/video1:/dev/video1 `# for webcam` \
-    allenyllee/motion-webcam-server
+    --name $CONTAINER_NAME \
+    --publish $OUT_PORT:8081 \
+    --device $VIDEO_PATH:/dev/video0 `# for webcam` \
+    allenyllee/motion-webcam-server \
+    motion
--- scripts/init.sh
+++ scripts/init.sh
@@ -7,4 +7,6 @@ SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 $SOURCE_DIR/setup_motion.sh
 
-bash
+# shell - Process all arguments except the first one (in a bash script) - Stack Overflow
+# https://stackoverflow.com/questions/9057387/process-all-arguments-except-the-first-one-in-a-bash-script
+bash -c "${@:1}"
